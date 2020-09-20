bbb7b4b4394042a6a8ac84e517cacab1b9591459
Hyun Ahn <codepawn@icloud.com>
2020-09-21 08:46:44 +0900

    streams

---
+++ lib/stream.exs
@@ -0,0 +1,9 @@
+odd? = &(rem(&1, 2) != 0)
+IO.inspect(odd?)
+res = 1..100 |> Stream.filter(odd?) |> Enum.sum()
+IO.inspect(res)
+res0 = 1..100 |> Enum.sum()
+IO.inspect(res0)
+
+res1 = 1..100_000 |> Enum.sum()
+IO.inspect(res1)
